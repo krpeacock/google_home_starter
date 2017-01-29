4a057a3a0e4838bf641617aee1d352d42dea47f0
Gavin Peacock <gpeacock@adobe.com>
2017-01-28 17:09:17 -0800

    fix tabs and spaces

--- public/python/switch.py
+++ public/python/switch.py
@@ -1,21 +1,21 @@
 # Switch ETEKCITY Zap outlet on an off via 433 hrtz transmitter.
 
 codes = [ 
-	{ on: 283955, off: 283964 },
-	{ on: 284099, off: 284108 },
-	{ on: 284419, off: 284428 },
-	{ on: 285955, off: 285964 },
-	{ on: 292099, off: 292108 }]
+	{ 'on': 283955, 'off': 283964 },
+	{ 'on': 284099, 'off': 284108 },
+	{ 'on': 284419, 'off': 284428 },
+	{ 'on': 285955, 'off': 285964 },
+	{ 'on': 292099, 'off': 292108 }]
 
 
 if __name__ == '__main__':
-    import sys
+	import sys
 
-    if len(sys.argv) == 1:
-        print 'Usage: %s switchnum state'
+	if len(sys.argv) == 1:
+		print 'Usage: %s switchnum state'
 
 	switchnum = sys.argv[1]
-    state = sys.argv[2]
+	state = sys.argv[2]
 
 	import pi_switch
 	sender = pi_switch.RCSwitchSender()
